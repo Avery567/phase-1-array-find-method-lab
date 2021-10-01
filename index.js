8c5744c3dcef790d1e0a86db98612e4c2e38b026
Avery Lin <tclin100@gmail.com>
2021-10-01 11:57:20 -0400

    commit tree 6bbdd4da5fa44600672f5ea4f75bd91457da283a

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function superbowlWin(record) {
+    const result = record.find(game => game.result === "W")
+    console.log(result)
+    if(result === undefined) {
+        return undefined
+    } else {return result.year}
+}
